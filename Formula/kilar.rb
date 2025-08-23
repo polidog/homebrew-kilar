2107e7c4a66eece0d184a4a8993241e14c3bac31
Ryota Mochizuki <polidogs@gmail.com>
2025-08-23 09:01:52 +0900

    Update kilar formula to version 0.1.2
    - Updated version from 0.1.1 to 0.1.2
    - Updated SHA256 checksums for all platforms

--- Formula/kilar.rb
+++ Formula/kilar.rb
@@ -1,26 +1,26 @@
 class Kilar < Formula
   desc "Powerful CLI tool for managing port processes"
   homepage "https://github.com/polidog/kilar"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/polidog/kilar/releases/download/v#{version}/kilar-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "e59be0721183dc2c6652ff93eda50421ff08687a45202e9ec2a522ca272bdb60"
+      sha256 "4684587da7440846a0c1a00ee582040f492b4a40fa848450959064b41467d1d8"
     else
       url "https://github.com/polidog/kilar/releases/download/v#{version}/kilar-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "3dc2839389c363915d037530e822e7d4c0fe9e74fa62565f63d591c0b21b58e4"
+      sha256 "8551c72756a40779df5d423f42d55a462fba82981714c2e226eba1aaa1afedbb"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/polidog/kilar/releases/download/v#{version}/kilar-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "ee07fb2101b32f5fcc712fee5c0a42a5060f4363267701459dabb032057d8abe"
+      sha256 "4dbd901ed3bea50ca045f9853014f7b0eceed0890459cee3c16cd60d134cf839"
     else
       # Linux ARM version is not currently available
-      odie "Linux ARM64 version is not available. Please use x86_64 version or build from source."
+      odie "Linux ARM24 version is not available. Please use x86_64 version or build from source."
     end
   end
 
